1d81cf97160433e852f9c89566a9bdb55c82a193
hyunwoo00 <96560273+kodingzz@users.noreply.github.com>
2022-09-15 10:51:35 +0900

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+let js = 'amazing';
+if (js === 'amazing') alert('javascript is fun!');
+
+console.log(23 + 43);
+
+let firstName = "Matilda";
+console.log(firstName);
+
+let PI = 3.1415;
+let $function = 25;
+
+let country = "South korea";
+let continent = "Asia";
+let population = 5;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+console.log(typeof PI);
+console.log(typeof continent);
+
+javascriptIsFun = 'yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
