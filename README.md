fce9d2d410c6702ffbf810ec21a271dffc3664af
hyunwoo00 <96560273+kodingzz@users.noreply.github.com>
2022-09-16 21:31:17 +0900

    js-fundamental-1

--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript-study
-자바스크립트 공부
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 if (js === 'amazing') alert('javascript is fun!');
 
@@ -28,3 +29,84 @@ console.log(typeof javascriptIsFun);
 
 let year;
 console.log(year);
+
+
+let hw = 'hyunwoo';
+
+hw = 'kim hyun woo';
+console.log(hw);
+
+const ar = 'aron';
+ar = 'kim aron';  // const로 선언된변수는 변경 불가능
+console.log(ar);
+
+const now = 2022;
+const agehw = now - 2000;
+const agear = now - 2006;
+
+console.log(agehw, agear);
+console.log(agehw + 2, agear * 3);
+
+const firstName = 'Kim';
+const lastName = 'hyunwoo';
+console.log(firstName + ' ' + lastName);
+
+console.log(now - 2000, now - 2006);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+
+
+const firstName = 'Jonas';
+const year = 2022;
+const birthyear = 2000;
+const job = 'teacher';
+const jonasNew = `I'm ${firstName}, a ${year - birthyear} year olds ${job}`;
+console.log(jonasNew);
+
+const age = 15;
+const oldenough = age >= 18;
+
+if (oldenough) {
+    console.log('You can start driving license👨');
+}
+else {
+    const yearLeft = 18 - age;
+    console.log(`You is too young. Wait another ${yearLeft} years`);
+}
+const birthyear = 2000;
+let century;
+if (birthyear <= 2000) {
+    century = 20;
+}
+else {
+    century = 21;
+}
+console.log(century);
+
+const markWeight = 78;
+const markHeight = 1.69;
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const markBMI = markWeight / (markHeight ** 2);
+const johnBMI = johnWeight / (johnHeight ** 2);
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})`);
+}
+else {
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})`);
+}
+*/
+const inputYear = `1991`;
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 16);
+console.log(Number(inputYear) + 16);
+console.log(`I am 23 years old`);
+console.log('23' - '10' - 3);
+
